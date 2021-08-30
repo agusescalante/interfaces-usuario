afdb4180e1003aa6d6dd80e4e176581c16be5c5f
agusescalante <agustinescalante27@gmail.com>
2021-08-30 14:41:16 -0300

    create function ej2

--- practico1/ej2/main.js
+++ practico1/ej2/main.js
@@ -1,40 +1,28 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-//fila base
-ctx.fillStyle = '#550000';
-ctx.fillRect(300,225,100,75);
+const height = 75;
+const width = 100; 
+  
+colores = ['#550000','#801515','#D46A6A','#07123A'];
 
-ctx.fillStyle = '#801515';
-ctx.fillRect(200,225,100,75);
+function createRec(x,y,color,height,width){
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,height,width);
+}
 
-ctx.fillStyle = '#D46A6A';
-ctx.fillRect(100,225,100,75);
+createRec(300,225,colores[0],100,75);
+createRec(200,225,colores[1],100,75);
+createRec(100,225,colores[2],100,75);
+createRec(0,225,colores[3],100,75);
 
-ctx.fillStyle = '#07123A';
-ctx.fillRect(0,225,100,75);
+createRec(50,150,colores[3],100,75);
+createRec(150,150,colores[2],100,75);
+createRec(250,150,colores[1],100,75);
 
-//prox a la fila base
-ctx.fillStyle = '#07123A';
-ctx.fillRect(50,150,100,75);
+createRec(100,75,colores[3],100,75);
+createRec(200,75,colores[2],100,75);
 
-ctx.fillStyle = '#D46A6A';
-ctx.fillRect(150,150,100,75);
-
-ctx.fillStyle = '#801515';
-ctx.fillRect(250,150,100,75);
-
-
-//prox a la fila base
-ctx.fillStyle = '#07123A';
-ctx.fillRect(100,75,100,75);
-
-ctx.fillStyle = '#D46A6A';
-ctx.fillRect(200,75,100,75);
-
-
-//prox a la fila base
-ctx.fillStyle = '#07123A';
-ctx.fillRect(150,0,100,75);
+createRec(150,0,colores[3],100,75);
 
 
