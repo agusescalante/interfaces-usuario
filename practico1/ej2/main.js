2182bd90298968c8b1e82a96c27a35ee3941bfc3
agusescalante <agustinescalante27@gmail.com>
2021-08-30 18:42:09 -0300

    ej de practico1

--- practico1/ej2/main.js
+++ practico1/ej2/main.js
@@ -1,10 +1,11 @@
+"use strict";
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const height = 75;
 const width = 100; 
   
-colores = ['#550000','#801515','#D46A6A','#07123A'];
+let colores = ['#550000','#801515','#D46A6A','#07123A'];
 
 function createRec(x,y,color,height,width){
     ctx.fillStyle = color;
@@ -24,5 +25,4 @@ createRec(100,75,colores[3],100,75);
 createRec(200,75,colores[2],100,75);
 
 createRec(150,0,colores[3],100,75);
-
 
--- practico1/ej5/main.js
+++ practico1/ej5/main.js
@@ -1,9 +1,9 @@
-
+"use strict";
 let canvas = document.querySelector('.canvas');
 let ctx = canvas.getContext('2d');
 
-let height = canvas.height;
-let width = canvas.width;
+let width = 900;
+let height = 500;
 let imageData = ctx.createImageData(width,height);
 
 let r=0;
@@ -11,25 +11,26 @@ let g=0;
 let b=0;
 let a=255;
 
-for(let x = 0;x < height; x++){
-    let media = 255 / (height/2)
-    for(let y=0; y < width; y++){
-        if(x < width / 2){
+for(let x = 0;x < width; x++){
+    for(let y=0; y < height; y++){
+        var media = 255 / (width/2);
+        if(x < (width / 2)){
             r = media * x;
             g = media * x;
+            b = 0;
         }else{
-            //g = 255 - (media * (x - (width/2)));
-            r = 255 - (media * (x - (width/2)));
-
+            r = 255;
+            g = 255 - (media * (x - (width/2)));
         }
         setPixel(imageData,x,y,r,g,b,a);
     }
-    ctx.putImageData(imageData,0,0);
 }
+ctx.putImageData(imageData,0,0);
 
 
 
 function setPixel(imageData,x,y,r,g,b,a){
+    let index;
     index = (x + y * imageData.width)*4;
     imageData.data[index+0]=r;
     imageData.data[index+1]=g;
