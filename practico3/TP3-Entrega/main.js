4da6e709080f637363ea10284e4d39f9f1d139fd
agusescalante <agustinescalante27@gmail.com>
2021-10-19 20:27:40 -0300

    animation jump avatar

--- practico3/TP3-Entrega/main.js
+++ practico3/TP3-Entrega/main.js
@@ -1,38 +1,31 @@
 let avatar = document.getElementById("avatar");
 let rock = document.getElementById("rock");
 let score = document.getElementById("score");
-
-
+let keyDown = false;
 
 function jump() {
-  avatar.style.
-  avatar.classList.add("jump-animation");
+    avatar.classList.add("jump-animation");
     setTimeout(() =>
-        avatar.classList.remove("jump-animation"), 500);
-}
-
-document.addEventListener('keypress', (event) => {
-    if (!avatar.classList.contains('jump-animation')) {
-    jump();
-  }
-})
-
-setInterval(() => {
-  const avatarTop = parseInt(window.getComputedStyle(avatar)
-    .getPropertyValue('top'));
-  const rockLeft = parseInt(window.getComputedStyle(rock)
-    .getPropertyValue('left'));
-//  score.innerText++;
-
-  if (rockLeft < 0) {
-    rock.style.display = 'none';
-  } else {
-    rock.style.display = ''
+      avatar.classList.remove("jump-animation"), 500); 
   }
 
-  if (rockLeft < 50 && rockLeft > 0 && avatarTop > 350) {
-    // alert("You got a score of: " + score.innerText +
-    //   "\n\nPlay again?");
-    // location.reload();
-  }
-}, 50);
+    avatar.classList.add("run");
+
+  window.addEventListener('keydown', (event) => {
+      keyDown = true;
+  });
+
+  window.addEventListener("keyup",function(){
+    keyDown = false;
+    avatar.classList.add("run");
+
+  });
+
+  setInterval(()=>{
+    if(keyDown){
+        avatar.classList.remove("run");
+        jump();
+    }
+  },50);
+
+
