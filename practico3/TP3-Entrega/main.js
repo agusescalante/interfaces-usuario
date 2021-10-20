4b3856f5cc4714efdfdb5d3324442de946aa492b
agusescalante <agustinescalante27@gmail.com>
2021-10-20 20:36:07 -0300

    change img and animation keyDown

--- practico3/TP3-Entrega/main.js
+++ practico3/TP3-Entrega/main.js
@@ -1,32 +1,37 @@
 let avatar = document.getElementById("avatar");
 let rock = document.getElementById("rock");
 let score = document.getElementById("score");
-let keyDown = false;
+let tecla = false;
+let imgJump = 'url(images/jump-right.png)';
+let imgRun = 'url(images/spritestrip-.png)';
+//38 arriba
+//40 abajo
 
-function jump() {
-    avatar.classList.add("jump-animation");
-    setTimeout(() =>
-      avatar.classList.remove("jump-animation"), 500); 
-  }
+avatar.style.backgroundImage = imgRun;
+   avatar.classList.add("run");
 
-    avatar.classList.add("run");
-
-  window.addEventListener('keydown', (event) => {
-      keyDown = true;
+  window.addEventListener('keydown', (e) => {
+    tecla = e.keyCode;
+   // avatar.classList.remove("jump-animation");
   });
 
   window.addEventListener("keyup",function(){
-    keyDown = false;
-    avatar.classList.add("run");
+    tecla = null;
+     avatar.classList.add("run");
+    avatar.style.backgroundImage = imgRun;
 
   });
+      setInterval(()=>{
+        if(tecla === 38){
+          avatar.style.backgroundImage = imgJump;
+          avatar.classList.remove("run");
+          avatar.classList.add("jump-animation");
+        }
+        // else if(tecla == 40){
+        //     //animation agacharse
+        // }
 
-  setInterval(()=>{
-    if(keyDown){
-        avatar.classList.remove("run");
-        jump();
-    }
-  },50);
+      },70);
 
   //test
 
